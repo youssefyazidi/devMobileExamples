e73f569c6828b5a60c5b91211c01988719f165ad
alaoui <yazidi17@gmail.com>
2023-04-13 18:09:50 +0000

    13042023

---
+++ activite7/app/src/main/java/com/example/activite7/CityActivity.java
@@ -0,0 +1,24 @@
+package com.example.activite7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CityActivity extends AppCompatActivity {
+
+    TextView textviewnompays;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+
+        textviewnompays = findViewById(R.id.textviewPays);
+
+        Intent intent = getIntent();
+        String nompays= intent.getStringExtra("country");
+
+        textviewnompays.setText(nompays);
+    }
+}
---
+++ activite7/app/src/main/java/com/example/activite7/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.activite7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnStart;
+    EditText nameedittext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnStart = findViewById(R.id.btnStart);
+        nameedittext = findViewById(R.id.nompaysEdittext);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameedittext.getText().toString();
+                Toast.makeText(MainActivity.this,
+                        "nom de pays "+ name, Toast.LENGTH_SHORT).show();
+
+                startExploring(name);
+            }
+        });
+    }
+
+    private void startExploring(String name)
+    {
+        Intent intent =new Intent(this,CityActivity.class);
+        intent.putExtra("country", name);
+        startActivity(intent);
+    }
+}
